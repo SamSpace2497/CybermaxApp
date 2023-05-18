a2a3a2f87f9976321d1d37f947f91c6bd68cf39f
Shubham Patil <118250165+SamSpace2497@users.noreply.github.com>
2023-05-18 13:42:02 +0530

    Add project files.

---
+++ CybermaxApp/Controllers/UserController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CybermaxApp.Data;
+using CybermaxApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CybermaxApp.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+              return _context.User != null ? 
+                          View(await _context.User.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.User'  is null.");
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var userEntity = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(userEntity);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Mobile,Email")] UserEntity userEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userEntity);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var userEntity = await _context.User.FindAsync(id);
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+            return View(userEntity);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Mobile,Email")] UserEntity userEntity)
+        {
+            if (id != userEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserEntityExists(userEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userEntity);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var userEntity = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(userEntity);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.User == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.User'  is null.");
+            }
+            var userEntity = await _context.User.FindAsync(id);
+            if (userEntity != null)
+            {
+                _context.User.Remove(userEntity);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserEntityExists(int id)
+        {
+          return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CybermaxApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CybermaxApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CybermaxApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<UserEntity> User { get; set; }
+
+    }
+}
